516912a5b11bd8a27e835a36627da8a26c61050b
erickspringer-maker <erickspringer@bastostigre.adv.br>
2026-08-31 22:06:04 -0300

    Adiciona service worker offline

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'tabuada-2-v3';
+const APP_SHELL = [
+  './', './index.html', './manifest.webmanifest', './icon.svg',
+  './style-01.part', './style-02.part',
+  './app-01.part', './app-02.part', './app-03.part', './app-04.part', './app-05.part', './app-06.part'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
